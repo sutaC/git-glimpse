ccb0f54d68e7219961dde0fd0869cdbf84861114
sutaC <catus.pl@gmail.com>
2026-01-11 01:55:15 +0100

    Path traversal

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from flask import Flask, render_template, abort
 
-REPO_PATH =  Path(__file__).parent / "repo"
+REPO_PATH =  Path(__file__).parent.parent / "repo" # project_root / repo
 if(not REPO_PATH.is_dir()): REPO_PATH.mkdir()
 
 app = Flask(__name__)
@@ -10,23 +10,29 @@ app = Flask(__name__)
 def root():
     return render_template("index.html", msg="Hello World!")
 
-@app.route("/repo/<string:id>", defaults={"subpath": ""}, strict_slashes=False)
-@app.route("/repo/<string:id>/<path:subpath>")
-def repo(id: str, subpath: str):
+@app.route("/repo/<string:id>", defaults={"sub": ""}, strict_slashes=False)
+@app.route("/repo/<string:id>/<path:sub>")
+def repo(id: str, sub: str):
     # TODO check for id scheme
     if len(id) != 8:
         abort(404)
-    pth = REPO_PATH / id
-    if not pth.is_dir():
+    path = REPO_PATH / id
+    if not path.is_dir():
         abort(404)
-    pth /= Path(subpath)
-    if not pth.exists():
+    subpath = Path(sub)
+    path /= subpath
+    if not path.exists() or ".git" in path.parts:
         abort(404)
-        
+    str(subpath).lstrip(".")
+    pathchain = []
+    # for part in Path(subpath).parts:
+
+
     return render_template(
         "repo.html", 
-        name="name", 
-        pathname=str(Path(subpath)).lstrip("."),
-        path=pth,
-        parentchain=subpath.split("/")[:-1]
+        name="repo-name", 
+        pathname=str(subpath).lstrip("."),
+        path=path,
+        id=id,
+        parentchain=path.parts[path.parts.index(id)+1:-1]
     )
