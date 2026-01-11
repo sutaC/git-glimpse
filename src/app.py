5ac507c155baf14d44cfd935cf7544b1bb078273
sutaC <catus.pl@gmail.com>
2026-01-11 03:21:21 +0100

    components

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-from flask import Flask, render_template, abort
+from flask import Flask, Response, render_template, abort, send_file, redirect
 
-REPO_PATH =  Path(__file__).parent.parent / "repo" # project_root / repo
+REPO_PATH =  Path(__file__).parent.parent / "repo" # /[...]/project_root/repo
 if(not REPO_PATH.is_dir()): REPO_PATH.mkdir()
 
 app = Flask(__name__)
@@ -20,15 +20,36 @@ def repo(id: str, sub: str):
     if not path.is_dir():
         abort(404)
     subpath = Path(sub)
-    path /= subpath
-    if not path.exists() or ".git" in path.parts:
+    path = (path / subpath).resolve()
+    if not path.exists() or ".git" in path.parts or REPO_PATH not in path.parents:
         abort(404)
-    str(subpath).lstrip(".")
+    # Makes list of path urls to all parent dirs
+    parentchain = path.parts[path.parts.index(id)+1:-1]
+    parentchain = ['/'.join(parentchain[:parentchain.index(p)+1]) for p in parentchain]
+    # ---
     return render_template(
         "repo.html", 
-        name="repo-name", 
-        pathname=str(subpath).lstrip("."),
+        repo_name="repo-name", 
+        path_str=str(subpath).lstrip("."),
         path=path,
         id=id,
-        parentchain=path.parts[path.parts.index(id)+1:-1]
+        parent_chain=parentchain
     )
+
+@app.route("/raw/<string:id>/<path:sub>")
+def raw(id: str, sub: str):
+    # TODO check for id scheme
+    if len(id) != 8:
+        abort(404)
+    path = REPO_PATH / id
+    if not path.is_dir():
+        abort(404)
+    subpath = Path(sub)
+    path = (path / subpath).resolve()
+    if not path.exists() or ".git" in path.parts or REPO_PATH not in path.parents:
+        abort(404)
+    if path.is_dir():
+        return redirect(f"/repo/{id}/{sub}", 303)
+    with open(path, "r", encoding="utf-8", errors="replace") as f:
+        return Response(f.read(), mimetype="text/plain")
+
